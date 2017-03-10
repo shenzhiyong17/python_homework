01fed90c1e5bd14a0289f9c8d8bc38437b17d808
shenzhiyong17 <no194@163.com>
2017-03-10 18:13:19 +0800

    deap bugfix

--- dataStructure/tree/heap/deap.py
+++ dataStructure/tree/heap/deap.py
@@ -14,14 +14,14 @@ class Deap(CompletelyTree):
         CompletelyTree.__init__(self)
         self.b_tree.append(Node(' '))  # 根，即b_tree[1]为空
 
-    def max_heap(self, index):
+    def __max_heap(self, index):
         while index > 3:
             index /= 2
         if index == 2:
             return False
         return True
 
-    def partner(self, left=0, right=0):
+    def __partner(self, left=0, right=0):
         # 查找对应节点位置
         if left > 1:
             t = int(math.log(left, 2) - 1)
@@ -37,7 +37,7 @@ class Deap(CompletelyTree):
             return left
         raise
 
-    def min_insert(self, index, key):
+    def __min_insert(self, index, key):
         if index == 2:
             try:
                 self.b_tree[index] = Node(key)
@@ -53,7 +53,7 @@ class Deap(CompletelyTree):
             parent /= 2
         self.b_tree[index] = Node(key)
 
-    def max_insert(self, index, key):
+    def __max_insert(self, index, key):
         if index == 3:
             try:
                 self.b_tree[index] = Node(key)
@@ -69,31 +69,28 @@ class Deap(CompletelyTree):
             parent /= 2
         self.b_tree[index] = Node(key)
 
-    def insert(self, index, key):
+    def __insert(self, index, key):
         parent = index / 2
-        if parent < 2:
-            self.b_tree[index] = Node(key)
-            return
         if not self.b_tree[parent]:
             raise
-        if self.max_heap(index):
-            i = self.partner(right=index)
+        if self.__max_heap(index):
+            i = self.__partner(right=index)
             if not self.is_in(index * 2) and self.is_in(i * 2):
                 i *= 2
                 if self.is_in(i + 1) and self.b_tree[i] < self.b_tree[i + 1]:
                     i += 1
             if self.b_tree[i].key > key:
                 self.b_tree[index] = self.b_tree[i]
-                self.min_insert(i, key)
+                self.__min_insert(i, key)
             else:
-                self.max_insert(index, key)
+                self.__max_insert(index, key)
         else:
-            j = self.partner(left=index)
+            j = self.__partner(left=index)
             if self.b_tree[j].key < key:
                 self.b_tree[index] = self.b_tree[j]
-                self.max_insert(j, key)
+                self.__max_insert(j, key)
             else:
-                self.min_insert(index, key)
+                self.__min_insert(index, key)
 
     def append(self, key):
         if self.len() == 2:  # 最小堆根节点
@@ -106,23 +103,25 @@ class Deap(CompletelyTree):
                 self.b_tree.append(Node(key))
             return
         index = self.len()
-        if self.max_heap(index):
-            i = self.partner(right=index)
+        if self.__max_heap(index):
+            i = self.__partner(right=index)
             if self.b_tree[i].key > key:
                 self.b_tree.append(self.b_tree[i])
-                self.min_insert(i, key)  # 把原先的i替换成新node，然后适配
+                self.__min_insert(i, key)  # 把原先的i替换成新node，然后适配
             else:
-                self.max_insert(index, key)  # append在最后，然后适配
+                self.__max_insert(index, key)  # append在最后，然后适配
         else:
-            j = self.partner(left=index)
+            j = self.__partner(left=index)
             if self.b_tree[j].key < key:
                 self.b_tree.append(self.b_tree[j])
-                self.max_insert(j, key)
+                self.__max_insert(j, key)
             else:
-                self.min_insert(index, key)
+                self.__min_insert(index, key)
 
     def delete_min(self):
-        last = self.b_tree.pop(-1).key
+        last = self.b_tree.pop(-1)
+        if self.len() <= 2:
+            return last
         res = self.b_tree[2]
         index = 4
         while self.is_in(index):
@@ -133,11 +132,13 @@ class Deap(CompletelyTree):
             self.b_tree[parent] = self.b_tree[index]
             index *= 2
         index /= 2
-        self.insert(index, last)
+        self.__insert(index, last.key)
         return res
 
     def delete_max(self):
-        last = self.b_tree.pop(-1).key
+        last = self.b_tree.pop(-1)
+        if self.len() <= 3:
+            return last
         res = self.b_tree[3]
         index = 6
         while self.is_in(index):
@@ -148,19 +149,30 @@ class Deap(CompletelyTree):
             self.b_tree[parent] = self.b_tree[index]
             index *= 2
         index /= 2
-        self.insert(index, last)
+        self.__insert(index, last.key)
         return res
 
     def test(self):
-        # rand = [6, 60, 67, 85, 18, 3, 98, 23, 35, 48, 60, 82, 30, 84, 45, 47, 75, 57, 46, 32]
+        # rand = [65, 13, 96, 96, 39, 35, 51, 23, 60, 66, 90, 38, 74, 67, 33, 14, 31, 88, 68, 74]
         print rand
+
+        # 降序测试
         for v in rand:
             self.append(v)
-        self.print_tree()
-        print 'delete_min: %s' % self.delete_min()
-        self.print_tree()
-        print 'delete_max: %s' % self.delete_max()
-        self.print_tree()
+        order = []
+        while self.len() > 2:
+            order.append(self.delete_max())
+        sort = sorted(rand, reverse=True)
+        assert sort == self.list(order)
+
+        # 升序测试
+        for v in rand:
+            self.append(v)
+        order = []
+        while self.len() > 2:
+            order.append(self.delete_min())
+        sort = sorted(rand, reverse=False)
+        assert sort == self.list(order)
 
 
 if __name__ == '__main__':

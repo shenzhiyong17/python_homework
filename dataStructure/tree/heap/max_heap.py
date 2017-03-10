e26a9cb4e459149f560988a077561fe8fe2a7db2
shenzhiyong17 <no194@163.com>
2017-03-10 16:40:26 +0800

    MaxHeap bugfix and add test function in MaxHeap

--- dataStructure/tree/heap/max_heap.py
+++ dataStructure/tree/heap/max_heap.py
@@ -2,58 +2,58 @@
 # -*- coding: utf-8 -*-
 # date: 2016-2-3
 
+from dataStructure.common.test_data import rand
 from dataStructure.tree.completely_tree import *
 
 
 class MaxHeap(CompletelyTree):
     ## 最大堆，page 137，from《数据结构》
 
-    def append(self, key):
+    def add(self, key):
         node = Node(key)
         self.b_tree.append(node)
-        tail = len(self.b_tree) - 1
+        tail = self.len() - 1
         while tail > 1:
             parent = tail / 2
-            # print self.b_tree[parent], self.b_tree[tail]
-            if self.b_tree[parent].key >= self.b_tree[tail].key:
+            if self.b_tree[parent] >= self.b_tree[tail]:
                 break
             self.b_tree[tail], self.b_tree[parent] = self.b_tree[parent], self.b_tree[tail]
             tail = parent
         self.b_tree[tail] = node
 
     def pop(self):
-        if self.len() == 0:
+        if self.len() <= 1:
             return None
-        item = self.b_tree[1]
+        if self.len() == 2:
+            return self.b_tree.pop(-1)
+        max = self.b_tree[1]
+        tmp = self.b_tree.pop()
         tail = self.len() - 1
-        tmp = self.b_tree[tail]
-        self.b_tree.pop()
-        tail -= 1
         parent = 1
         child = 2
-        while (child < tail):
-            if child < tail and self.left(parent).key < self.right(parent).key:
+        while (child <= tail):
+            if self.is_in(child + 1) and self.b_tree[child] < self.b_tree[child + 1]:
                 child += 1
-            if tmp.key >= self.b_tree[child].key:
+            if tmp >= self.b_tree[child]:
                 break
             self.b_tree[parent] = self.b_tree[child]
             parent = child
             child *= 2
         self.b_tree[parent] = tmp
-        return item
+        return max
 
     def test(self):
-        rand_list = gen_rand_list(14, 1, 999)
-        print rand_list
-        for i in rand_list:
-            self.append(i)
-            # self.print_tree()
-            # print '-----'
+        # rand = [82, 61, 80, 50, 66, 5, 84, 6, 94, 99, 61, 6, 73, 69, 24, 55, 77, 77, 26, 21]
+        # rand = [6,5,6]
+        print rand
+        for i in rand:
+            self.add(i)
 
-        print self.pop().key
-        self.print_tree()
-        print self.pop().key
-        self.print_tree()
+        order = []
+        while self.len() > 1:
+            order.append(self.pop())
+        sort = sorted(rand, reverse=True)
+        assert sort == self.list(order)
 
 
 class Heapify(CompletelyTree):
@@ -103,22 +103,21 @@ class Heapify(CompletelyTree):
         return head
 
     def test(self):
-        rand_list = gen_rand_list(10, 0, 500)
-        rand_list.insert(0, None)
-        print rand_list
-        self.build_heapify(rand_list)
+        rand.insert(0, None)
+        print rand
+        self.build_heapify(rand)
         for i in self.b_tree:
             print i,
         print ''
         self.print_tree()
         for i in range(3):
             print self.pop()
-            self.print_tree()
+        self.print_tree()
 
 
 if __name__ == '__main__':
     bt = MaxHeap()
     bt.test()
 
-    bt = Heapify()
-    bt.test()
+    # bt = Heapify()
+    # bt.test()

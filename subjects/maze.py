cd00a555ce6c3c9a45a7467ad0f4d6962c7da5b3
shenzhiyong17 <no194@163.com>
2017-04-18 16:38:26 +0800

    add maze

--- subjects/maze.py
+++ subjects/maze.py
@@ -3,29 +3,79 @@
 # date: 2017-04-17
 
 import random
-import tools.colorFormat as colorFormat
+import time
+
+from tools.colorFormat import color_format
 
 
-def maze(h, v, p):  # 迷宫大小h x v. p为百分比.
-    m = []
-    for i in range(v):
-        m.append([1, ] * h)
-    for i in range(1, v - 1):
-        for j in range(1, h - 1):
-            if random.randint(0, 100) < p:
-                m[i][j] = 0
-    m[0][1] = m[v -1][h-2] = 0
-    return m
+class Maze():
+    def __init__(self, long, width, per=75):  # 迷宫大小x * y. p为百分比.
+        # {(x,y)=(v,m)..}    x,y-坐标,v-0为通道1为墙,m-面包屑
+        self.maze = {}
+        for x in range(long):
+            for y in range(width):
+                self.maze[(x, y)] = (1, False)
+        for x in range(1, long - 1):
+            for y in range(1, width - 1):
+                if random.randint(0, 100) < per:
+                    self.maze[(x, y)] = (0, False)
+        self.entrance = (1, 1)
+        self.exit = (long - 2, width - 2)
+        self.maze[self.entrance] = (0, False)
+        self.maze[self.exit] = (0, False)
+        self.path = [self.entrance,]
+        self.long = long
+        self.width = width
 
+    def printmaze(self):
+        for x in range(self.long):
+            for y in range(self.width):
+                v, m = self.maze[(x, y)]
+                if v == 0:
+                    v = color_format(v, mode='highlight')
+                print v,
+            print '\n'
 
-def printm(maze):
-    for line in maze:
-        for item in line:
-            if item == 0:
-                item = colorFormat.color_format(item,mode='highlight')
-            print item,
-        print '\n'
+    def printpath(self):
+        for x in range(self.long):
+            for y in range(self.width):
+                v, m = self.maze[(x, y)]
+                if (x, y) in self.path:
+                    v = color_format(v, mode='highlight')
+                print v,
+            print '\n'
+
+    def solvem(self):
+        p = self.entrance
+        while p != self.exit:
+            x, y = p
+            move = 0
+            for n in ((x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)):
+                if self.maze[n] == (0, False):
+                    self.maze[n] = (0, True)
+                    if not (p in self.path):
+                        self.path.append(p)
+                    p = n
+                    self.path.append(p)
+                    move = 1
+                    break
+            if self.path == []:
+                raise RuntimeError('no way')
+            if move == 0:
+                p = self.path.pop(-1)
+        return self.path
 
 
 if __name__ == '__main__':
-    printm(maze(20, 10, 70))
+    while True:
+        try:
+            maze = Maze(5, 10, 80)
+            # time.sleep(0.1)
+            print maze.solvem()
+            maze.printmaze()
+            print '========='
+            maze.printpath()
+            break
+        except RuntimeError:
+            print '==========='
+            continue

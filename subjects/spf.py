79f6cf343b64f3e16118297ab335df65d25b6aa9
shenzhiyong17 <no194@163.com>
2017-05-16 16:37:17 +0800

    优化spf

--- subjects/spf.py
+++ subjects/spf.py
@@ -15,11 +15,19 @@ class Node:
         self.pos = (x, y)
         self.blank = blank
         self.map = {self.pos: {'cost': 0, 'nexthop': self.pos}}
+        self.neibor = []
 
     def __str__(self):
         return "%s:%s map:%s" % (self.pos, self.blank, str(self.map))
 
+    def __eq__(self, other):
+        if other:
+            return self.pos == other.pos
+        return False
+
     def neighbor(self):
+        if self.neibor:
+            return self.neibor
         neighbor = []
         x, y = self.pos
         for h, r in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
@@ -27,25 +35,31 @@ class Node:
             if h in range(self.maze.long) and r in range(self.maze.width):
                 if self.maze[pos].blank:
                     neighbor.append(self.maze[pos])
+        self.neibor = neighbor
         return neighbor
 
-    def set_map(self, pos, cost, nexthop):         # 更新map后要广播更新
-        if not pos in self.map:
-            self.map[pos] = {'cost': cost, 'nexthop': nexthop}
-            self.broadcast(pos, cost)
+    def set_map(self, dst, cost, nexthop):         # 更新map后要广播更新
+        if not dst in self.map:
+            self.map[dst] = {'cost': cost, 'nexthop': nexthop}
+            self.broadcast(dst, cost)
         else:
-            if cost < self.map[pos]['cost']:
-                self.map[pos] = {'cost': cost, 'nexthop': nexthop}
-                self.broadcast(pos, cost)
+            if cost < self.map[dst]['cost']:
+                self.map[dst] = {'cost': cost, 'nexthop': nexthop}
+                self.broadcast(dst, cost)
 
-    def update(self, pos, cost, node):          # 收到消息后更新自己的map
-        self.set_map(pos, cost + 1, node.pos)
+    def update(self, dst, cost, nexthop):          # 收到消息后更新自己的map
+        if self.pos == self.maze.entrance or len(self.neighbor()) > 2:
+            self.set_map(dst, cost + 1, nexthop.pos)
+        else:
+            self.broadcast(dst, cost + 1, exclude=nexthop)
 
-    def broadcast(self, pos=None, cost=0):      # 广播自己的map中某一节点的信息
-        if not pos:
-            pos = self.pos
+    def broadcast(self, dst=None, cost=0, exclude=None):      # 广播自己的map中某一节点的信息
+        if not dst:
+            dst = self.pos
         for node in self.neighbor():
-            node.update(pos, cost, self)
+            if node == exclude:
+                continue
+            node.update(dst, cost, self)
 
 
 class Maze:
@@ -67,15 +81,27 @@ class Maze:
     def __getitem__(self, pos):
         return self.maze[pos]
 
-    def path(self):
-        p = self.maze[self.entrance]
+    def path(self, src, dst):
+        pre = p = self.maze[src]
         path = [p]
-        while p != self.maze[self.exit]:
-            p = self.maze[p.map[self.exit]['nexthop']]
+        while p != self.maze[dst]:
+            if dst in p.map.keys():
+                pre = p
+                p = self.maze[p.map[dst]['nexthop']]
+            else:
+                for n in p.neighbor():
+                    if n != pre:
+                        pre = p
+                        p = n
+                        break
             path.append(p)
         return path
 
-    def printpath(self, path):
+    def printpath(self, src, dst):
+        if dst in self.maze[src].map:
+            path = self.path(src, dst)
+        else:
+            raise RuntimeError('no way')
         head = '  '
         for y in range(self.width):
             head += ' %2s' % y
@@ -103,40 +129,31 @@ class Maze:
         entrance = self.maze[self.entrance]
         exit.broadcast()
         if self.exit in entrance.map:
-            print entrance.map
-            path = self.path()
-            self.printpath(path)
+            # print entrance.map
+            self.printpath(self.entrance, self.exit)
         else:
             raise RuntimeError('no way')
 
     def complete_convergence(self):         # 完全收敛,每个节点了解整张map
-        entrance = self.maze[self.entrance]
         for x in range(self.long):
             for y in range(self.width):
                 node = self.maze[(x, y)]
                 if node.blank:
                     node.broadcast()
-        if self.exit in entrance.map:
-            print '%s\'s map (%s): ' % (entrance.pos, len(entrance.map))
-            for pos in entrance.map.keys():
-                print '\t%s: %s' %(pos, entrance.map[pos])
-            path = self.path()
-            self.printpath(path)
-        else:
-            raise RuntimeError('no way')
 
 
 if __name__ == '__main__':
     cnt = 0
     while True:
         cnt += 1
-        print 'cnt: %s' %cnt
+        print cnt
         try:
-            m = Maze(25, 40, 65)
+            m = Maze(25, 40, 60)
             t1 = timing(m.run)[0]
             print 't1: %s' % t1
             t2 = timing(m.complete_convergence)[0]
             print 't2: %s' %t2
+            m.printpath(m.entrance, m.exit)
             break
         except RuntimeError:
             continue

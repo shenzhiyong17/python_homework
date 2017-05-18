14cf8592805b667d54901b5a27998e3665223733
shenzhiyong17 <no194@163.com>
2017-05-18 16:02:53 +0800

    M

--- subjects/solves/maze.py
+++ subjects/solves/maze.py
@@ -6,66 +6,91 @@
 import random
 
 from tools.colorFormat import color_format
+from common.timing import timing
 
 
-class Maze():
-    def __init__(self, long, width, per=75):  # 迷宫大小x * y. p为百分比.
+class Maze:
+    class Node:
+        def __init__(self, px, py, blank, maze):
+            self.maze = maze
+            self.pos = (px, py)
+            self.blank = blank
+            self.bread = False
+            self.neigh = None
+
+        def __str__(self):
+            return str(self.pos)
+
+        def __eq__(self, other):
+            if other:
+                return self.pos == other.pos
+            return False
+
+        def neighbor(self):
+            if self.neigh is not None:
+                return self.neigh
+            neighbor = []
+            px, py = self.pos
+            for h, r in ((px + 1, py), (px - 1, py), (px, py + 1), (px, py - 1)):
+                pos = (h, r)
+                if h in range(self.maze.long) and r in range(self.maze.width):
+                    if self.maze[pos].blank:
+                        neighbor.append(self.maze[pos])
+            self.neigh = neighbor
+            return neighbor
+
+    def __init__(self, sx, sy, per=75):  # 迷宫大小x * y. p为百分比.
         # {(x,y)=(v,m)..}    x,y-坐标,v-0为通道1为墙,m-面包屑
-        self.maze = {}
-        for x in range(long):
-            for y in range(width):
-                self.maze[(x, y)] = ('=', False)
-        for x in range(1, long - 1):
-            for y in range(1, width - 1):
+        self.map = {}
+        for x in range(sx):
+            for y in range(sy):
                 if random.randint(0, 100) < per:
-                    self.maze[(x, y)] = (0, False)
-        self.entrance = (1, 1)
-        self.exit = (long - 2, width - 2)
-        self.maze[self.entrance] = (0, False)
-        self.maze[self.exit] = (0, False)
-        self.long = long
-        self.width = width
+                    self.map[(x, y)] = self.Node(x, y, True, self)
+                else:
+                    self.map[(x, y)] = self.Node(x, y, False, self)
+        self.entrance = (0, 0)
+        self.exit = (sx - 1, sy - 1)
+        self.map[self.entrance].blank = True
+        self.map[self.exit].blank = True
+        self.long = sx
+        self.width = sy
+
+    def __getitem__(self, pos):
+        return self.map[pos]
 
     def printpath(self, path):
+        head = '  '
+        for y in range(self.width):
+            head += ' %2s' % y
+        print head
         for x in range(self.long):
+            print '%2s' % x,
             for y in range(self.width):
-                v, m = self.maze[(x, y)]
-                if (x, y) in path:
-                    v = color_format('X', mode='highlight')
-                elif v == 0:
-                    v = ' '
-                print v,
+                v = self.map[(x, y)]
+                if v in path:
+                    print color_format(' X', mode='highlight'),
+                elif v.blank:
+                    print '  ',
+                else:
+                    print ' =',
             print ''
         print '***************************'
 
-    def around(self, p):
-        x, y = p
-        return (x, y + 1), (x + 1, y), (x - 1, y), (x, y - 1)
-
     def solve(self):
         # 找出一条路径到达出口,然后简化掉其中环路
-        p = self.entrance
+        p = self.map[self.entrance]
         path = []
-        while p != self.exit:
+        while p.pos != self.exit:
             n = p
-            for d in self.around(p):
-                if self.maze[d] == (0, False):
-                    self.maze[d] = (0, True)
-                    n = d
-                    break
+            for d in p.neighbor():
+                if d.blank:
+                    if not d.bread:
+                        d.bread = True
+                        n = d
+                        break
             if n != p:
+                # print n,p
                 path.append(p)
-                # for around in self.around(n):       # 优化path,去掉环路
-                #     try:
-                #         if around != p and self.maze[around] == (0, True):
-                #             while True:
-                #                 if path.pop(-1) == around:
-                #                     break
-                #             path.append(around)
-                #     except KeyError:
-                #         continue
-                #     except IndexError:
-                #         raise RuntimeError('no way')
                 p = n
             elif not path:
                 raise RuntimeError('no way')
@@ -77,31 +102,26 @@ class Maze():
         index = 0
         while index < len(path) - 2:  # 优化path,去掉环路
             p = path[index]
-            for around in self.around(p):
-                if around in path[index + 2:]:
+            for nei in p.neighbor():
+                if nei in path[index + 2:]:
                     while True:
-                        if path.pop(index + 1) == around:
+                        if path.pop(index + 1) == nei:
                             break
-                    path.insert(index + 1, around)
+                    path.insert(index + 1, nei)
             index += 1
         return path
 
-    def solve1(self):
-        # 找出相对短的路由
-        # 如果下一步离目的更近则继续,否则切换到另一条路
-        pass
-
     def solve_all(self):
         # 深度优先策略,找出所有解
-        stack = [[self.entrance]]
+        stack = [[self.map[self.entrance]]]
         s = []
         while stack:
             path = stack.pop()
             if path == self.exit:
                 s.append(path)
             else:
-                for p in self.around(path[-1]):
-                    if not p in path and self.maze[p][0] == 0:
+                for p in path[-1].neighbor():
+                    if not p in path and self.map[p][0] == 0:
                         stack.append(path + [p])
         return s
 
@@ -112,10 +132,12 @@ if __name__ == '__main__':
     y = 80
     while True:
         try:
+            print count
             maze = Maze(x, y, 60)
-            path = maze.solve()
+            t, path = timing(maze.solve)
             print 'count: %s' % count
             maze.printpath(path)
+            print t
             break
         except RuntimeError:
             count += 1
--- subjects/spf.py
+++ subjects/spf.py
@@ -10,15 +10,15 @@ from common.timing import timing
 
 
 class Node:
-    def __init__(self, x, y, blank, maze):
-        self.maze = maze
+    def __init__(self, x, y, blank, map):
+        self.map = map
         self.pos = (x, y)
         self.blank = blank
-        self.map = {self.pos: {'cost': 0, 'nexthop': self.pos}}
-        self.neibor = []
+        self.route_table = {self.pos: {'cost': 0, 'nexthop': self.pos}}
+        self.neigh = []
 
     def __str__(self):
-        return "%s:%s map:%s" % (self.pos, self.blank, str(self.map))
+        return "%s:%s map:%s" % (self.pos, self.blank, str(self.route_table))
 
     def __eq__(self, other):
         if other:
@@ -26,32 +26,29 @@ class Node:
         return False
 
     def neighbor(self):
-        if self.neibor:
-            return self.neibor
+        if self.neigh:
+            return self.neigh
         neighbor = []
         x, y = self.pos
         for h, r in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
             pos = (h, r)
-            if h in range(self.maze.long) and r in range(self.maze.width):
-                if self.maze[pos].blank:
-                    neighbor.append(self.maze[pos])
-        self.neibor = neighbor
+            if h in range(self.map.long) and r in range(self.map.width):
+                if self.map[pos].blank:
+                    neighbor.append(self.map[pos])
+        self.neigh = neighbor
         return neighbor
 
-    def set_map(self, dst, cost, nexthop):         # 更新map后要广播更新
-        if not dst in self.map:
-            self.map[dst] = {'cost': cost, 'nexthop': nexthop}
+    def set_route(self, dst, cost, nexthop):         # 更新map后要广播更新
+        if not dst in self.route_table:
+            self.route_table[dst] = {'cost': cost, 'nexthop': nexthop}
             self.broadcast(dst, cost)
         else:
-            if cost < self.map[dst]['cost']:
-                self.map[dst] = {'cost': cost, 'nexthop': nexthop}
+            if cost < self.route_table[dst]['cost']:
+                self.route_table[dst] = {'cost': cost, 'nexthop': nexthop}
                 self.broadcast(dst, cost)
 
     def update(self, dst, cost, nexthop):          # 收到消息后更新自己的map
-        if self.pos == self.maze.entrance or len(self.neighbor()) > 2:
-            self.set_map(dst, cost + 1, nexthop.pos)
-        else:
-            self.broadcast(dst, cost + 1, exclude=nexthop)
+        self.set_route(dst, cost + 1, nexthop.pos)
 
     def broadcast(self, dst=None, cost=0, exclude=None):      # 广播自己的map中某一节点的信息
         if not dst:
@@ -62,43 +59,35 @@ class Node:
             node.update(dst, cost, self)
 
 
-class Maze:
+class Map:
     def __init__(self, long, width, per=75):    # 迷宫大小x * y. p为百分比.
         self.long = long
         self.width = width
-        self.maze = {}
+        self.map = {}
         for x in range(long):
             for y in range(width):
                 if random.randint(0, 100) < per:
-                    self.maze[(x, y)] = Node(x, y, True, self)  # 指定几率改为通路
+                    self.map[(x, y)] = Node(x, y, True, self)  # 指定几率改为通路
                 else:
-                    self.maze[(x, y)] = Node(x, y, False, self)
+                    self.map[(x, y)] = Node(x, y, False, self)
         self.entrance = (0, 0)
         self.exit = (long - 1, width - 1)
-        self.maze[self.entrance].blank = True
-        self.maze[self.exit].blank = True
+        self.map[self.entrance].blank = True
+        self.map[self.exit].blank = True
 
     def __getitem__(self, pos):
-        return self.maze[pos]
+        return self.map[pos]
 
     def path(self, src, dst):
-        pre = p = self.maze[src]
+        p = self.map[src]
         path = [p]
-        while p != self.maze[dst]:
-            if dst in p.map.keys():
-                pre = p
-                p = self.maze[p.map[dst]['nexthop']]
-            else:
-                for n in p.neighbor():
-                    if n != pre:
-                        pre = p
-                        p = n
-                        break
+        while p != self.map[dst]:
+            p = self.map[p.route_table[dst]['nexthop']]
             path.append(p)
         return path
 
     def printpath(self, src, dst):
-        if dst in self.maze[src].map:
+        if dst in self.map[src].route_table:
             path = self.path(src, dst)
         else:
             raise RuntimeError('no way')
@@ -110,7 +99,7 @@ class Maze:
             print '%2s' %x,
             for y in range(self.width):
                 try:
-                    v = self.maze[(x, y)]
+                    v = self.map[(x, y)]
                     if v in path:
                         v = color_format(' X', mode='highlight')
                     elif v.blank:
@@ -125,19 +114,18 @@ class Maze:
         print '***************************'
 
     def run(self):
-        exit = self.maze[self.exit]
-        entrance = self.maze[self.entrance]
+        exit = self.map[self.exit]
+        entrance = self.map[self.entrance]
         exit.broadcast()
-        if self.exit in entrance.map:
-            # print entrance.map
-            self.printpath(self.entrance, self.exit)
+        if self.exit in entrance.route_table:
+            return
         else:
             raise RuntimeError('no way')
 
     def complete_convergence(self):         # 完全收敛,每个节点了解整张map
         for x in range(self.long):
             for y in range(self.width):
-                node = self.maze[(x, y)]
+                node = self.map[(x, y)]
                 if node.blank:
                     node.broadcast()
 
@@ -148,12 +136,13 @@ if __name__ == '__main__':
         cnt += 1
         print cnt
         try:
-            m = Maze(25, 40, 60)
+            m = Map(25, 80, 60)
             t1 = timing(m.run)[0]
-            print 't1: %s' % t1
-            t2 = timing(m.complete_convergence)[0]
-            print 't2: %s' %t2
             m.printpath(m.entrance, m.exit)
+            print 't1: %s' % t1
+            # t2 = timing(m.complete_convergence)[0]
+            # print 't2: %s' %t2
+            # m.printpath(m.entrance, m.exit)
             break
         except RuntimeError:
             continue

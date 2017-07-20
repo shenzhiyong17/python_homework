9174288a5c29c6427237deeca3e657e9ce0e3800
shenzhiyong17 <no194@163.com>
2017-07-20 17:31:41 +0800

    M

--- subjects/web_server/fake_web_server.py
+++ subjects/web_server/fake_web_server.py
@@ -4,26 +4,26 @@
 
 import sys
 import web
+import urllib2
 from web.wsgiserver import CherryPyWSGIServer
 from common.IOHelper import *
 
 
 class Hello:
     def GET(self, path):
-        web.header('Access-Control-Allow-Credentials', 'true')
-        web.header('Access-Control-Allow-Origin', 'http://s.miwifi.com')
         i = web.input(name=None, cmd=None, info=None, appId=None)
-        client_ip = web.ctx.ip
         path = '/%s' % path
-        cfg_file = None
         result = None
         try:
             if i.appId:
-                if i.appId == '2882303761517410304':    # ebit
+                web.header('Access-Control-Allow-Credentials', 'true')
+                web.header('Access-Control-Allow-Origin', 'http://s.miwifi.com')
+                if i.appId == '2882303761517410304':  # ebit
                     cfg_file = '%s%s' % (project_dir, '/test_ebit/response_cfg/response')
                     cfg = load_config(cfg_file)
                     result = cfg[path]
-                    if path.find("feature_plugin_control"):
+                    print '--------------- result: %s' %result
+                    if path.endswith("feature_plugin_control"):
                         result = result[i.info]
                 elif i.appId == '2882303761517555697':  # ccgame
                     cfg_file = '%s%s' % (project_dir, '/test_ccgame/response_cfg/response')
@@ -37,6 +37,23 @@ class Hello:
                     result = cfg[path]
                     if path == "/api-third-party/service/internal/ipv6":
                         result = result[i.cmd]
+            else:
+                print "web.ctx.ip:%s" % web.ctx.ip  # client ip
+                print "web.ctx.host: %s" % web.ctx.host  # www.163.com
+                print "web.ctx.home: %s" % web.ctx.home  # http://www.163.com
+                print "web.ctx.homedomain: %s" % web.ctx.homedomain  # http://www.163.com
+                print "web.ctx.homepath: %s" % web.ctx.homepath  #
+                print "web.ctx.status: %s" % web.ctx.status  # 200 OK
+                print "web.ctx.headers: %s" % web.ctx.headers  # [('Access-Control-Allow-Credentials', 'true'), ('Access-Control-Allow-Origin', 'http://s.miwifi.com')]
+                print "web.ctx.method: %s" % web.ctx.method  # GET
+                print "web.ctx.query: %s" % web.ctx.query  # ?callback=latestInstantNews&d=1231
+                print "web.ctx.fullpath: %s" % web.ctx.fullpath  # /special/00774IVV/hot_pop_js2017.js?callback=latestInstantNews&d=1231
+                print "web.ctx.env: "
+                for ver in web.ctx.env:
+                    print "\t%s: %s" %(ver, web.ctx.env.get(ver))
+                print web.ctx.env.get('HTTP_REFERER', 'http://google.com')
+                url = "%s%s" % (web.ctx.home, web.ctx.fullpath)
+                return urllib2.urlopen(url).read()
         except Exception as e:
             print e
             return json.dumps({'msg': 'path not found!'}, ensure_ascii=False)
